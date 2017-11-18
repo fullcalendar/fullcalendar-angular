4eca43dcf076ebb00adff1a0e8905ae2a4599aa3
jamaks <jamakrus@gmail.com>
2017-11-18 21:57:31 +0300

    Update to version 1.1.0

--- src/lib/src/calendar.component.ts
+++ src/lib/src/calendar.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, OnInit, AfterViewInit, HostListener, AfterContentChecked, AfterViewChecked, ElementRef, EventEmitter } from '@angular/core';
-import * as $ from 'jquery';
+import $ from 'jquery';
 import 'fullcalendar';
 import { Options } from 'fullcalendar';
 import { ButtonClickModel } from './models/buttonClickModel';
