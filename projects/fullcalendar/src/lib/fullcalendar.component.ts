a060530bb370234a13fd6fd6f6443babb78b83cd
Adam Shaw <arshaw@users.noreply.github.com>
2020-05-01 16:24:33 -0400

    much more elegant solution to include fullcalendar-scheduler css

--- projects/fullcalendar/src/lib/fullcalendar.component.ts
+++ projects/fullcalendar/src/lib/fullcalendar.component.ts
@@ -16,7 +16,7 @@ import { OPTION_IS_DEEP } from './fullcalendar-options';
 @Component({
   selector: 'full-calendar',
   template: '',
-  styleUrls: ['./fullcalendar-scheduler.css'], // see <root>/scripts/scheduler-symlink.sh for more info
+  styleUrls: ['./fullcalendar.component.scss'],
   encapsulation: ViewEncapsulation.None // the styles are root-level, not scoped within the component
 })
 export class FullCalendarComponent implements AfterViewInit, DoCheck, AfterContentChecked, OnDestroy {
