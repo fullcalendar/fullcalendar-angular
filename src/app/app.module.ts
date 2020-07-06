c1e15655f7f1ea487424c19dac3280ff98f8008e
Ireneusz Budzowski <Ireneusz.Budzowski@billennium.com>
2020-07-06 18:37:45 +0200

    Reorder imports to make sure that fullCalendarModule is imported firstly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FullCalendarModule } from '@fullcalendar/angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FullCalendarModule } from '@fullcalendar/angular';
 
 @NgModule({
   declarations: [
