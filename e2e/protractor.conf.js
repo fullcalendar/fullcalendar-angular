f9545295f5370341ee817a589dfd40dab7cd6d1c
Cameron Steffen <cam.steffen94@gmail.com>
2022-05-10 11:19:40 -0500

    Fix jasmine displayStacktrace

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -23,6 +23,6 @@ exports.config = {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: 'raw' } }));
   }
 };
