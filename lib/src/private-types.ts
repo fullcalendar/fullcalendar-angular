135d8b148f44437724d7d018b551ad6e0e6512cf
arturovt <arthurandrosovich@gmail.com>
2023-04-07 13:44:05 +0300

    chore: format files and setup husky

--- lib/src/private-types.ts
+++ lib/src/private-types.ts
@@ -1,18 +1,19 @@
 import { TemplateRef } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core';
 
-export type CalendarOption<OptionName> = OptionName extends keyof CalendarOptions
-  ? CalendarOptions[OptionName]
-  : never
+export type CalendarOption<OptionName> =
+  OptionName extends keyof CalendarOptions
+    ? CalendarOptions[OptionName]
+    : never;
 
 export type CalendarTemplateRef<OptionName> = TemplateRef<{
-  $implicit: CalendarArgLookup<OptionName>
-}>
+  $implicit: CalendarArgLookup<OptionName>;
+}>;
 
 type CalendarArgLookup<OptionName> = OptionName extends keyof CalendarOptions
   ? FirstParam<CalendarOptions[OptionName]>
-  : never
+  : never;
 
-type FirstParam<Func> = Func extends ((...args: any) => any)
+type FirstParam<Func> = Func extends (...args: any) => any
   ? Parameters<Func>[0]
-  : never
+  : never;
--- lib/src/test.ts
+++ lib/src/test.ts
@@ -5,11 +5,15 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
     <T>(id: string): T;
     keys(): string[];
   };
@@ -18,7 +22,7 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
+  platformBrowserDynamicTesting()
 );
 
 // Then we find all the tests.
