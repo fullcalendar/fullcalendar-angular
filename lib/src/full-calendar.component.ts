670681cb38f9a89ee37f0e212453d679ee059aaf
Adam Shaw <arshaw@users.noreply.github.com>
2025-03-31 23:34:57 -0400

    simpler setup of customRenderingMetaMap, allowing TemplateRef usage

--- lib/src/full-calendar.component.ts
+++ lib/src/full-calendar.component.ts
@@ -59,7 +59,6 @@ export class FullCalendarComponent implements AfterViewInit, DoCheck, AfterConte
   private handleCustomRendering: (customRendering: CustomRendering<any>) => void
   private customRenderingMap = new Map<string, CustomRendering<any>>()
   private customRenderingArray?: CustomRendering<any>[]
-  public templateMap: { [templateName: string]: TemplateRef<any> } = {}
 
   constructor(
     private element: ElementRef,
@@ -74,7 +73,6 @@ export class FullCalendarComponent implements AfterViewInit, DoCheck, AfterConte
     });
 
     this.handleCustomRendering = customRenderingStore.handle.bind(customRenderingStore);
-    this.templateMap = this as any; // alias to this
   }
 
   ngAfterViewInit() {
@@ -210,7 +208,7 @@ export class FullCalendarComponent implements AfterViewInit, DoCheck, AfterConte
   private buildExtraOptions(): CalendarOptions {
     return {
       handleCustomRendering: this.handleCustomRendering,
-      customRenderingMetaMap: this.templateMap,
+      customRenderingMetaMap: this as unknown as { [templateName: string]: TemplateRef<any> },
       customRenderingReplaces: true,
     };
   }
